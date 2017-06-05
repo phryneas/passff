d3fd630f3f847bd530529bd74c410df8eb55fe22
Lenz Weber <mail@lenzw.de>
2017-06-05 23:39:50 +0200

    combine common files into common.js

--- Gruntfile.js
+++ Gruntfile.js
@@ -21,16 +21,34 @@ module.exports = function (grunt) {
         }
       },
     },
+    mkdir: {
+      all: {
+        options: {
+          create: ['built/content']
+        },
+      },
+    },
     browserify: {
       built: {
-        files: {
-          "./built/background.js": "built/ts/background.js",
-          "./built/content/popup.js": "built/ts/content/popup.js",
-          "./built/content/newPasswordWindow.js": "built/ts/content/newPasswordWindow.js",
-          "./built/content/preferencesWindow.js": "built/ts/content/preferencesWindow.js",
-        },
+        src: [
+          "built/ts/background.js",
+          "built/ts/content/popup.js",
+          "built/ts/content/newPasswordWindow.js",
+          "built/ts/content/preferencesWindow.js",
+        ],
+        dest: 'built/common.js',
         options: {
           browserifyOptions: {
+            plugin: [
+              ['factor-bundle', {
+                outputs: [
+                  "built/background.js",
+                  "built/content/popup.js",
+                  "built/content/newPasswordWindow.js",
+                  "built/content/preferencesWindow.js"
+                ]
+              }]
+            ],
             debug: false //  true for sourcemaps
           },
         }
@@ -58,6 +76,7 @@ module.exports = function (grunt) {
             expand: true,
             cwd: './built/',
             src: [
+              'common.js',
               'background.js',
               'content/*.js'
             ],
@@ -75,9 +94,10 @@ module.exports = function (grunt) {
 
   grunt.loadNpmTasks("grunt-browserify");
   grunt.loadNpmTasks("grunt-ts");
+  grunt.loadNpmTasks("grunt-mkdir");
   grunt.loadNpmTasks("grunt-contrib-compress");
   grunt.loadNpmTasks("grunt-contrib-watch");
 
   grunt.registerTask("default", ["watch"]);
-  grunt.registerTask("build", ["ts:built", "browserify:built", "compress:xpi"]);
+  grunt.registerTask("build", ["ts:built", "mkdir:all", "browserify:built", "compress:xpi"]);
 };
